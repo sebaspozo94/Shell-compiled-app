d51c5e017023fb129a8ff793ee3d18ecc82b6ce9
sebaspozo94 <sebaspozo94@gmail.com>
2026-03-07 18:53:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -347,7 +347,7 @@ if st.session_state.run_finished:
     with col_cam:
         view_choice = st.selectbox("🎥 Camera View", ["Default", "Bottom (XY)", "Front (XZ)", "Side (YZ)"])
     with col_scale:
-        use_true_scale = st.checkbox("📏 True Z-Scale", value=False)
+        use_true_scale = st.checkbox("📏 True Z-Scale", value=True)
         if use_true_scale:
             z_scale_pct = int(100*tmax/max(dimx, dimy))
         else:
@@ -374,7 +374,7 @@ if st.session_state.run_finished:
     Z_plot_neg = -Z_final 
     custom_colorscale = [[0.0, '#08306b'], [0.4, '#2563eb'], [1.0, '#cbd5e1']]
 
-    roof_surface = go.Surface(z=np.zeros_like(Z_plot_neg), x=X_mesh, y=Y_mesh, colorscale=[[0, '#cbd5e1'], [1, '#cbd5e1']], showscale=False, hoverinfo='skip')
+    roof_surface = go.Surface(z=np.zeros_like(Z_plot_neg), x=X_mesh, y=Y_mesh, colorscale=[[0, '#cbd5e1'], [1, '#cbd5e1']], showscale=False, hoverinfo='skip',opacity=0.5)
     
     bottom_surface = go.Surface(
         z=Z_plot_neg, 
@@ -450,6 +450,7 @@ if st.session_state.run_finished:
 
     stl_data = generate_stl(X_mesh, Y_mesh, Z_plot_neg)
     st.download_button(label="📥 Download as .STL File", data=stl_data, file_name=f"Optimized_Slab_Iter{idx}.stl", mime="model/stl", type="primary")
+
 
 
 
